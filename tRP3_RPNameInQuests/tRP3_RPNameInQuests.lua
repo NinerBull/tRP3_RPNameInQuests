c24c2113c9623ffa8fdd2671afbf8d9e444e7abb
NinerBull <72740816+NinerBull@users.noreply.github.com>
2024-05-15 23:24:11 +0200

    Version Bump

--- tRP3_RPNameInQuests/tRP3_RPNameInQuests.lua
+++ tRP3_RPNameInQuests/tRP3_RPNameInQuests.lua
@@ -1387,7 +1387,7 @@ end
 TRP3_API.module.registerModule({
 	name = "RP Name in Quest Text",
 	description = "Enhances questing immersion by putting your TRP3 Character Name (and optionally Race and Class) into Quest Text!",
-	version = "1.2.14",
+	version = "1.2.15",
 	id = "trp3_rpnameinquests",
 	onStart = TRP3RPNameInQuests_Init,
 	minVersion = 110,
